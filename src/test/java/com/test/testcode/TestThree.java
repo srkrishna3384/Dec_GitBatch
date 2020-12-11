8d9d1a46fbf4d013867b0d281316d39103ffdc59
srkrishna3384 <srkrishna3384@gmail.com>
2020-12-11 09:25:30 +0530

    Added TestThree.java

---
+++ src/test/java/com/test/testcode/TestThree.java
@@ -0,0 +1,12 @@
+package com.test.testcode;
+
+import org.testng.annotations.Test;
+
+public class TestThree {
+	@Test
+	public void methodthree() {
+		System.out.println("TestThree calss....");
+	}
+	
+
+}
